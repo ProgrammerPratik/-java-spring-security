543d5f5b9101c9e949269973f70dc663c4c325d7
Pratik Merekar <programmerpratik>
2024-12-12 15:05:35 +0530

    c

---
+++ src/main/java/com/example/securing_web/WebApplication.java
@@ -0,0 +1,10 @@
+package com.example.securing_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class WebApplication {
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+}
